d650c0d44317f2e85522ed74139c84701ed83c8c
imaiyan <enzoredivocanella@hotmail.com>
2024-09-26 15:02:29 -0300

    feat: using envs for redirect setting and port

--- main.go
+++ main.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"net/http"
+	"os"
 )
 
 func main() {
+
+	url := os.Getenv("REDIRECT_URL")
+	port := os.Getenv("LISTENING_PORT")
+
 	mux := http.NewServeMux()
 
-	mux.Handle("/", http.RedirectHandler("https://www.google.com/", http.StatusSeeOther))
+	mux.Handle("/", http.RedirectHandler(url, http.StatusSeeOther))
 
-	http.ListenAndServe(":8080", mux)
+	http.ListenAndServe(":"+port, mux)
 }
